3c406e85b847c1d85ae0a2281cc13288ccebff16
marrmutee <dzakyainur@github.com>
2025-11-12 16:34:08 +0800

    ketiga

--- autentikasi.py
+++ autentikasi.py
@@ -1,5 +1,9 @@
 def login():
     print("selamat datang ")
+    username = input("Masukkan Username: ")
+    password = input("Masukkan Password: ")
+    
+    return username 
 
-    
-    
+
+login()
