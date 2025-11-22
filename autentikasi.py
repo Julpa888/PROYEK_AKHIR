63c7685be0cdf0865842a0310286f563fe974fd6
marrmutee <dzakyainur@github.com>
2025-11-22 14:29:43 +0800

    gabut

--- autentikasi.py
+++ autentikasi.py
@@ -1,27 +1,24 @@
-import json
 import pwinput
-from storage import Storage
+from storage import load_users, save_users, load_admin
 
 def registrasi():
-    storage = Storage()
-    users = storage.load_users()
-    admin = storage.load_admin()
+    storage_users = load_users()
+    admin = load_admin()
     print("\n=== REGISTRASI AKUN ===")
     while True:
         username = input("Masukkan username baru: ").strip()
-        if username in users or username in admin:
+        if username in storage_users or username in admin:
             print("Username sudah terdaftar, coba lagi!")
             continue
         password = pwinput.pwinput("Masukkan password: ")
-        users[username] = {"password": password, "watchlist": []}
-        storage.save_users(users)
+        storage_users[username] = {"password": password, "watchlist": []}
+        save_users(storage_users)
         print("Registrasi berhasil! Silakan login.")
         break
 
 def login():
-    storage = Storage()
-    users = storage.load_users()
-    admin = storage.load_admin()
+    storage_users = load_users()
+    admin = load_admin()
     print("\n=== LOGIN SISTEM ===")
     while True:
         username = input("Masukkan username: ").strip()
@@ -29,9 +26,7 @@ def login():
         if username in admin and password == admin[username]:
             print("Login berhasil sebagai ADMIN!")
             return username, True
-        if username in users and password == users[username]["password"]:
+        if username in storage_users and password == storage_users[username]["password"]:
             print("Login berhasil sebagai USER!")
             return username, False
         print("Input data akun tidak valid! Coba lagi...\n")
-
-# testing
