b2c515029f0a53aadfa3584b8ead0ed1bfd7d30d
Nuril Akmal <akmalnuril65@gmail.com>
2025-11-25 13:39:56 +0800

    finally

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-#!/usr/bin/env python
+
 """
 K-Blacklist: Platform Streaming Drama Korea
 Main entry point - run from project root
 """
-
 import sys
 import os
-
-# Add src directory to Python path
 src_path = os.path.join(os.path.dirname(__file__), 'src')
 sys.path.insert(0, src_path)
-
-# Import and run main
 if __name__ == "__main__":
     from src.main import main
     main()
